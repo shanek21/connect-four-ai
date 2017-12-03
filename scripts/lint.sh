4cf7c759a7546fb206174c7fbdb42843cb09b1d2
David Abrahams <david.abrahams@students.olin.edu>
2017-12-03 14:02:53 -0500

    3: add second linter

---
+++ scripts/lint.sh
@@ -0,0 +1,6 @@
+echo Running cpplint...
+cpplint --recursive src/ include/
+echo
+echo Running cppcheck...
+# cppcheck doesn't know about build in c++ libraries, this supresses warnings
+cppcheck --enable=all --suppress=missingIncludeSystem src/ include/
