6e416aa1fdb249ebd07cfff26ac12f10bcb0836a
David Abrahams <david.abrahams@students.olin.edu>
2017-12-05 15:45:16 -0500

    11: add dummy function to test

---
+++ include/add_numbers.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_ADD_NUMBERS_H_
+#define INCLUDE_ADD_NUMBERS_H_
+
+int add_numbers(const int a, const int b);
+
+#endif  // INCLUDE_ADD_NUMBERS_H_
---
+++ src/add_numbers.cpp
@@ -0,0 +1,5 @@
+#include "../include/add_numbers.h"
+
+int add_numbers(const int a, const int b) {
+  return a + b;
+}
