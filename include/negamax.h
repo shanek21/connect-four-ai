1d7337b256e973be2a2305ec9c104445cf5c0f85
David Abrahams <david.abrahams@students.olin.edu>
2017-12-09 16:16:23 -0500

    10: add negamax implementation

---
+++ include/negamax.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_NEGAMAX_H_
+#define INCLUDE_NEGAMAX_H_
+
+int negamax(const State S);
+
+#endif  // INCLUDE_NEGAMAX_H_
---
+++ src/negamax.cpp
@@ -0,0 +1,33 @@
+#include "../include/negamax.h";
+#include "../include/board.h";
+
+/**
+ * @param S - the current state to evaluate
+ * @returns 1 if the next player to play can win, 0 if the game is a draw, -1
+ *   if the next player to play will lose
+ */
+int negamax(const State S) {
+  if (S.isBoardFull())  // if the game is over, it's a draw
+    return 0;
+
+  int width = State::WIDTH;
+
+  for (int x = 0; x < width; x++) {
+    if (S.isPlayable(x) && S.isWinningPlay(x)) {
+      // If there is a winning move, return 1
+      return 1;
+    }
+  }
+
+  // Assume current player will lose
+  int bestScore = -1;
+
+  for (int x = 0; x < width; x++) {
+    if (!S.isPlayable(x)) continue;
+    State nextState = S.play(x);
+    // Negative scores for our opponent are positive for us
+    bestScore = std::max(bestScore, -negamax(nextState));
+    if (bestScore == 1) return 1;  // if we've found a winning move, return
+  }
+  return bestScore;
+}
