3b5ffbb46de84c27d00a6cadd771d027e3103de2
David Abrahams <david.abrahams@students.olin.edu>
2017-12-11 00:15:12 -0500

    2: remove add_numbers

--- include/add_numbers.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_ADD_NUMBERS_H_
-#define INCLUDE_ADD_NUMBERS_H_
-
-int add_numbers(const int a, const int b);
-
-#endif  // INCLUDE_ADD_NUMBERS_H_
--- test/add_numbers.cpp
+++
@@ -1,8 +0,0 @@
-#include "../lib/catch.hpp"
-#include "../include/add_numbers.h"
-
-
-TEST_CASE("Numbers are added together") {
-  REQUIRE(add_numbers(1, 2) == 3);
-  REQUIRE(add_numbers(5, 3) == 8);
-}
