81142d95aae5ba64a431804e3fb1d158e3226323
David Abrahams <david.abrahams@students.olin.edu>
2017-12-11 18:00:49 -0500

    19: add working implementation of alpha beta pruning

--- src/negamax.cpp
+++ src/negamax.cpp
@@ -1,10 +1,11 @@
 #include <limits>
+#include <stdexcept>
+#include <iostream>
 #include "../include/negamax.h"
 #include "../include/state.h"
 
 int negamax(const State S) {
-  return negamax(S, std::numeric_limits<int>::min(),
-      std::numeric_limits<int>::max());
+  return negamax(S, -S.getBoardSize(), S.getBoardSize());
 }
 
 /**
@@ -14,16 +15,27 @@ int negamax(const State S) {
  *   moves_until_loss - board_size - 1
  */
 int negamax(const State s, int lowerBoard, int upperBound) {
-  if (s.isBoardFull())  // if the game is over, it's a draw
+  if (s.isBoardFull()) {  // if the game is over, it's a draw
     return 0;
+  }
 
   int width = State::WIDTH;
 
   for (int col = 0; col < width; col++) {
     if (s.isPlayable(col) && s.isWinningPlay(s.getNextTileColor(), col)) {
-      // If there is a winning move, return 1
+      // If there is a winning move
       return S.getBoardSize() - S.getNumMoves();
     }
+  }
+
+  // Best case scenario, we win after our opponents next move
+  int bestPossibleScore = std::max(S.getBoardSize() - S.getNumMoves() - 2, 0);
+  // Can't do better than bestPossibleScore
+  upperBound = std::min(upperBound, bestPossibleScore);
+  // If elsewhere we've found a higher lower bound, then assume this position
+  // has the best possible score, since it will be discarded anyway
+  if (lowerBound >= upperBound) {
+    return upperBound;
   }
 
   // Assume current player will lose
@@ -33,7 +45,15 @@ int negamax(const State s, int lowerBoard, int upperBound) {
     if (!s.isPlayable(col)) continue;
     State nextState = s.play(col);
     // Negative scores for our opponent are positive for us
-    bestScore = std::max(bestScore, -negamax(nextState));
+    bestScore = std::max(bestScore,
+        -negamax(nextState, -upperBound, -lowerBound));
+    // If this score is higher than the lowerBound, increase the bound
+    lowerBound = std::max(lowerBound, bestScore);
+    if (upperBound <= lowerBound) {
+      // If the upperBound is <= the current lower bound, then this state is
+      //   unreachable, so return bestScore (this node will be discarded)
+      return bestScore;
+    }
   }
   return bestScore;
 }
--- test/negamax.cpp
+++ test/negamax.cpp
@@ -8,17 +8,24 @@ TEST_CASE("negamax()") {
     REQUIRE(negamax(S) < 0);
     S = State::boardFromNums("3575316255751336464276636772271112");
     REQUIRE(negamax(S) < 0);
+    S = State::boardFromNums("12514726155174536522772517671");
+    REQUIRE(negamax(S) < 0);
   }
   SECTION("Winning") {
     State S = State::boardFromNums("7422341735647741166133573473242566");
     REQUIRE(negamax(S) > 0);
     S = State::boardFromNums("326615663752621323655335514271");
     REQUIRE(negamax(S) > 0);
-    /* S = State::boardFromNums(""); */
-    /* REQUIRE(negamax(S) == 1); */
+    // This case is too slow
+    /* S = State::boardFromNums("24617524315172127"); */
+    /* REQUIRE(negamax(S) > 0); */
   }
   SECTION("Draw") {
-    State S = State::boardFromNums("23163416124767223154467471272416755633");
+    State S = State::boardFromNums("74425337641465475671176741236615215533");
+    REQUIRE(negamax(S) == 0);
+    S = State::boardFromNums("34441157374153736716346556774311552222");
+    REQUIRE(negamax(S) == 0);
+    S = State::boardFromNums("23163416124767223154467471272416755633");
     REQUIRE(negamax(S) == 0);
     S = State::boardFromNums("7172212567451542223676134464437761515");
     REQUIRE(negamax(S) == 0);
