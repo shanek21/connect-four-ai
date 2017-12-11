936343f167b0425dca7db6dcb71b4b6a3dbefc59
David Abrahams <david.abrahams@students.olin.edu>
2017-12-11 17:10:31 -0500

    19: calc number of moves to win/lose

--- src/negamax.cpp
+++ src/negamax.cpp
@@ -3,8 +3,9 @@
 
 /**
  * @param S - the current state to evaluate
- * @returns 1 if the next player to play can win, 0 if the game is a draw, -1
- *   if the next player to play will lose
+ * @returns if the game is winning, board_size - moves_until_win + 1.
+ *   If the game is a draw, 0. If the game is losing, return
+ *   moves_until_loss - board_size - 1
  */
 int negamax(const State s) {
   if (s.isBoardFull()) return 0;  // if the game is over, it's a draw
@@ -14,19 +15,18 @@ int negamax(const State s) {
   for (int col = 0; col < width; col++) {
     if (s.isPlayable(col) && s.isWinningPlay(s.getNextTileColor(), col)) {
       // If there is a winning move, return 1
-      return 1;
+      return S.getBoardSize() - S.getNumMoves();
     }
   }
 
   // Assume current player will lose
-  int bestScore = -1;
+  int bestScore = S.getNumMoves() - S.getBoardSize();
 
   for (int col = 0; col < width; col++) {
     if (!s.isPlayable(col)) continue;
     State nextState = s.play(col);
     // Negative scores for our opponent are positive for us
     bestScore = std::max(bestScore, -negamax(nextState));
-    if (bestScore == 1) return 1;  // if we've found a winning move, return
   }
   return bestScore;
 }
