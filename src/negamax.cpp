eb0f723d9ccb523e38ade5f88a7336f71b2fb5be
David Abrahams <david.abrahams@students.olin.edu>
2017-12-12 00:18:31 -0500

    19: add inside-out move order

--- src/negamax.cpp
+++ src/negamax.cpp
@@ -4,7 +4,19 @@
 #include "../include/negamax.h"
 #include "../include/state.h"
 
-int negamax(const State S) {
+Solver::Solver() {
+  int w = State::WIDTH;
+  int i = (w - 1) / 2;
+  int j = w / 2;
+  moveOrder.push_back(i);
+  if (i != j) moveOrder.push_back(j);
+  while (i > 0) {
+    moveOrder.push_back(--i);
+    moveOrder.push_back(++j);
+  }
+}
+
+int Solver::negamax(State S) {
   return negamax(S, -S.getBoardSize(), S.getBoardSize());
 }
 
@@ -14,14 +26,13 @@ int negamax(const State S) {
  *   If the game is a draw, 0. If the game is losing, return
  *   moves_until_loss - board_size - 1
  */
-int negamax(const State S, int lowerBound, int upperBound) {
+int Solver::negamax(State S, int lowerBound, int upperBound) {
   if (S.isBoardFull()) {  // if the game is over, it's a draw
     return 0;
   }
 
-  int width = State::WIDTH;
-
-  for (int x = 0; x < width; x++) {
+  for (auto it = moveOrder.begin(); it != moveOrder.end(); it++) {
+    int x = *it;
     if (S.isPlayable(x) && S.isWinningPlay(S.getNextTileColor(), x)) {
       // If there is a winning move
       return S.getBoardSize() - S.getNumMoves();
@@ -32,16 +43,17 @@ int negamax(const State S, int lowerBound, int upperBound) {
   int bestPossibleScore = std::max(S.getBoardSize() - S.getNumMoves() - 2, 0);
   // Can't do better than bestPossibleScore
   upperBound = std::min(upperBound, bestPossibleScore);
-  // If elsewhere we've found a higher lower bound, then assume this position
-  // has the best possible score, since it will be discarded anyway
   if (lowerBound >= upperBound) {
+    // If elsewhere we've found a higher lower bound, then assume this position
+    // has the best possible score, since it will be discarded anyway
     return upperBound;
   }
 
   // Assume current player will lose
   int bestScore = S.getNumMoves() - S.getBoardSize();
 
-  for (int x = 0; x < width; x++) {
+  for (auto it = moveOrder.begin(); it != moveOrder.end(); it++) {
+    int x = *it;
     if (!S.isPlayable(x)) continue;
     State nextState = S.play(x);
     // Negative scores for our opponent are positive for us
