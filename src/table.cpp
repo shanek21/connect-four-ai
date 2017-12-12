a50629fd6b28aa86c8d219f190b6a7ff2a0259ec
David Abrahams <david.abrahams@students.olin.edu>
2017-12-12 15:41:05 -0500

    25: implement table, and store board states

--- src/table.cpp
+++ src/table.cpp
@@ -5,9 +5,21 @@
 
 Table::Table(uint32_t numBits) {
   int entryMem = sizeof(Entry);
-  std::cout << entryMem << std::endl;
   // TODO(davidabrahams) remove this
   assert(entryMem == 8);  // should be 8 bytes
   int storedBoards = numBits / sizeof(Entry);
-  T.reserve(storedBoards);
+  std::cout << storedBoards << std::endl;
+  table.resize(storedBoards, {0, 0});  // fill vector with all-0 structs
 }
+
+uint32_t Table::getTableIndex(uint64_t board) const {
+  return board % table.size();
+}
+
+void Table::put(State::TileBoard board, uint8_t score) {
+  uint64_t boardInt = board.to_ulong();
+  uint32_t tableIndex = getTableIndex(boardInt);
+  table[tableIndex].board = boardInt;
+  table[tableIndex].score = score;
+}
+
