cf5cb52705ae149e7587a4afe3dab032a8df525e
David Abrahams <david.abrahams@students.olin.edu>
2017-12-12 17:47:25 -0500

    28: add binary search for alpha-beta window, and more tests

--- test/solver.cpp
+++ test/solver.cpp
@@ -14,7 +14,10 @@ TEST_CASE("negamax broken", "[!mayfail]") {
     REQUIRE(solver.score(S) < 0);
     S = State::boardFromNums("12514726155174536522772517671");
     REQUIRE(solver.score(S) < 0);
+    S = State::boardFromNums("43745416472735");
+    REQUIRE(solver.score(S) == -13);
   }
+
   SECTION("Winning") {
     State S = State::boardFromNums("7422341735647741166133573473242566");
     REQUIRE(solver.score(S) > 0);
@@ -22,6 +25,12 @@ TEST_CASE("negamax broken", "[!mayfail]") {
     REQUIRE(solver.score(S) > 0);
     S = State::boardFromNums("24617524315172127");
     REQUIRE(solver.score(S) > 0);
+    // This takes about 4 seconds
+    /* S = State::boardFromNums("7234472553"); */
+    /* REQUIRE(solver.score(S) == 10); */
+    // This is unsolvable (for now)
+    /* S = State::boardFromNums("266674777"); */
+    /* REQUIRE(solver.score(S) == 4); */
   }
   SECTION("Draw") {
     State S = State::boardFromNums("74425337641465475671176741236615215533");
