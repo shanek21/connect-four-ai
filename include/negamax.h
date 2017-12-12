9aaf1fa7dec8799e804c9ddcc353530268a92599
David Abrahams <david.abrahams@students.olin.edu>
2017-12-12 00:18:31 -0500

    19: add inside-out move order

--- include/negamax.h
+++ include/negamax.h
@@ -1,9 +1,17 @@
 #ifndef INCLUDE_NEGAMAX_H_
 #define INCLUDE_NEGAMAX_H_
 
+#include <list>
 #include "../include/state.h"
 
-int negamax(const State S);
-int negamax(const State S, int lowerBoard, int upperBound);
+class Solver {
+ public:
+  Solver();
+  int negamax(State S);
+  int negamax(State S, int lowerBound, int upperBound);
+ protected:
+  std::list<int> moveOrder;
+};
+
 
 #endif  // INCLUDE_NEGAMAX_H_
