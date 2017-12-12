1f25e455e91f1847773f07a648f1d4962ac8f7d7
William Lu <william.a.lu@gmail.com>
2017-12-11 19:32:22 -0500

    changed bkgd and tile colors

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -12,14 +12,14 @@ void displayGrid(State s) {
     for (int col = 0; col < State::WIDTH; col++) {
       State::TileType tileType = s.getTile(row, col);
       if (tileType == State::Red) {
-        attron(COLOR_PAIR(1));
-        mvaddch(Y_OFFSET + row + 1, X_OFFSET + tileOffsets[col], 'O');
-        attroff(COLOR_PAIR(1));
+        attron(COLOR_PAIR(1) | A_BOLD);
+        mvaddch(Y_OFFSET + row + 1, X_OFFSET + tileOffsets[col], '@');
+        attroff(COLOR_PAIR(1) | A_BOLD);
       }
       if (tileType == State::Black) {
-        attron(COLOR_PAIR(2));
-        mvaddch(Y_OFFSET + row + 1, X_OFFSET + tileOffsets[col], 'O');
-        attroff(COLOR_PAIR(2));
+        attron(COLOR_PAIR(2) | A_BOLD);
+        mvaddch(Y_OFFSET + row + 1, X_OFFSET + tileOffsets[col], '@');
+        attroff(COLOR_PAIR(2) | A_BOLD);
       }
     }
   }
@@ -32,8 +32,10 @@ void setUpScreen() {
   initscr();                                // Start curses mode
   start_color();
   noecho();
-  init_pair(1, COLOR_RED, COLOR_BLACK);     // Creates red tiles
-  init_pair(2, COLOR_YELLOW, COLOR_BLACK);  // Creates yellow tiles
+  init_pair(1, COLOR_RED, COLOR_WHITE);     // Creates red tiles
+  init_pair(2, COLOR_BLACK, COLOR_WHITE);   // Creates yellow tiles
+  init_pair(3, 244, COLOR_WHITE);
+  bkgd(COLOR_PAIR(3));
   mvaddstr(Y_OFFSET, X_OFFSET, "=============================");
   for (int y = Y_OFFSET + 1; y <= Y_OFFSET + State::HEIGHT + 1; y++) {
     mvaddstr(y, X_OFFSET, "|   |   |   |   |   |   |   |");
