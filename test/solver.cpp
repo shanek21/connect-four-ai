a9e863a26001027ae63cbd59d717287d3045868e
David Abrahams <david.abrahams@students.olin.edu>
2017-12-13 00:44:00 -0500

    28: add broken test cases

--- test/solver.cpp
+++ test/solver.cpp
@@ -6,51 +6,16 @@ TEST_CASE("negamax broken", "[!mayfail]") {
   Solver solver;
   SECTION("Broken0") {
     State S = State::boardFromNums("1667675535724753771415352132");
-    REQUIRE(solver.negamax(S) == 1);
-  SECTION("Losing") {
-    State S = State::boardFromNums("2252576253462244111563365343671351441");
-    REQUIRE(solver.score(S) < 0);
-    S = State::boardFromNums("3575316255751336464276636772271112");
-    REQUIRE(solver.score(S) < 0);
-    S = State::boardFromNums("12514726155174536522772517671");
-    REQUIRE(solver.score(S) < 0);
-    S = State::boardFromNums("43745416472735");
-    REQUIRE(solver.score(S) == -13);
+    REQUIRE(solver.score(S) == 1);
   }
-
-  SECTION("Winning") {
-    State S = State::boardFromNums("7422341735647741166133573473242566");
-    REQUIRE(solver.score(S) > 0);
-    S = State::boardFromNums("326615663752621323655335514271");
-    REQUIRE(solver.score(S) > 0);
-    S = State::boardFromNums("24617524315172127");
-    REQUIRE(solver.score(S) > 0);
-    // This takes about 4 seconds
-    /* S = State::boardFromNums("7234472553"); */
-    /* REQUIRE(solver.score(S) == 10); */
-    // This is unsolvable (for now)
-    /* S = State::boardFromNums("266674777"); */
-    /* REQUIRE(solver.score(S) == 4); */
+  SECTION("Broken1") {
+    State S = State::boardFromNums("427631264721");
+    REQUIRE(solver.score(S) == 10);
   }
-  SECTION("Draw") {
-    State S = State::boardFromNums("74425337641465475671176741236615215533");
-    REQUIRE(solver.score(S) == 0);
-    S = State::boardFromNums("34441157374153736716346556774311552222");
-    REQUIRE(solver.score(S) == 0);
-    S = State::boardFromNums("23163416124767223154467471272416755633");
-    REQUIRE(solver.score(S) == 0);
-    S = State::boardFromNums("7172212567451542223676134464437761515");
-    REQUIRE(solver.score(S) == 0);
+  SECTION("Broken2") {
+    State S = State::boardFromNums("11163142533736577");
+    REQUIRE(solver.score(S) == 2);
   }
-  // These test cases might fail, but for now they're too slow
-  /* SECTION("Broken1") { */
-  /*   State S = State::boardFromNums("427631264721"); */
-  /*   REQUIRE(solver.negamax(S) == 10); */
-  /* } */
-  /* SECTION("Broken2") { */
-  /*   State S = State::boardFromNums("11163142533736577"); */
-  /*   REQUIRE(solver.negamax(S) == 2); */
-  /* } */
 }
 
 TEST_CASE("negamax()") {
